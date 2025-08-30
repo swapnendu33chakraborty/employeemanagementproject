848e4b1704be02978eb4243f9e7e89395531f182
Swapnendu Chakraborty <166035235+swapnendu33chakraborty@users.noreply.github.com>
2025-08-30 15:23:40 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Authcontext from './context/Authcontext.jsx'
+import Taskcontext from './context/Taskcontext.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+  <Authcontext>
+  <Taskcontext>  
+    <App />
+  </Taskcontext>
+  </Authcontext>
+  </StrictMode>,
+)
